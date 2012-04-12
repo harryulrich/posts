7ee82f2f6510cce450c9aaf8848315f62b358a20
Harry Ulrich <harry.ulrich@celerityinnovation.com>
2012-04-12 13:20:17 -0400

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Posts::Application.config.secret_token = 'e000e30b723af26e75036f1bb8c5892cbe1e3f677e1e64114d442eaf78c4ecd47bdf62709cfc13451fb2ff1761e2878649c4a7cfea2bbc4fcb8542b80d76916d'
